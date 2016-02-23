928457bc4a04d6a448833fcf5ba7107e904e3e38
Bob Green <bob@9r33n.net>
2016-02-23 18:19:31 -0500

    Add exploration cursor

--- client/client.js
+++ client/client.js
@@ -1,108 +1,116 @@
 
 var players = ["bob","joe","jbz","peter","snorp"];
 
-d3.select("#lobby-players")
-    .selectAll("p")
-    .data(players)
-    .enter()
-    .append("p")
-    .text(function(d) { return d; });
-
 var probColor = d3.scale.quantize().domain([1, 100]).range(["#4575b4","#91bfdb","#e0f3f8","#fee090","#fc8d59","#d73027"]);
 var costColor = d3.scale.quantize().domain([1, 25]).range(["#4575b4","#91bfdb","#e0f3f8","#fee090","#fc8d59","#d73027"]);
 var taxColor = d3.scale.quantize().domain([100, 550]).range(["#4575b4","#91bfdb","#e0f3f8","#fee090","#fc8d59","#d73027"]);
 var oilColor = d3.scale.quantize().domain([1, 9]).range(["#4d4d4d",,"#878787","#bababa","#e0e0e0","#ffffff","#fddbc7","#f4a582","#d6604d","#b2182b"]);
 
-function lobbyState(game) {
+function play() {
+    d3.select("#lobby-players")
+        .selectAll("p")
+        .data(players)
+        .enter()
+        .append("p")
+        .text(function(d) { return d; });
+
+    return lobby
+}
+
+function lobby() {
     $("#lobby").hide()
 
     $.post("/game/0/player/0/", '{"done":true}', function(data) {
         d3.select("#prob")
-           .append("svg")
-           .selectAll("rect")
-           .data(data.prob)
-           .enter()
-           .append("rect")
-           .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
-           .attr("x", function (d, i) { return i%80 * 12 ; })
-           .style("fill", probColor);
+            .selectAll("rect")
+            .data(data.prob)
+            .enter()
+            .append("rect")
+            .attr("class", function (d, i) { return "site-" + i; })
+            .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
+            .attr("x", function (d, i) { return i%80 * 12 ; })
+            .style("fill", probColor);
 
-       d3.select("#cost")
-          .append("svg")
-          .selectAll("rect")
-          .data(data.cost)
-          .enter()
-          .append("rect")
-          .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
-          .attr("x", function (d, i) { return i%80 * 12 ; })
-          .style("fill", costColor);
+        d3.select("#cost")
+            .selectAll("rect")
+            .data(data.cost)
+            .enter()
+            .append("rect")
+            .attr("class", function (d, i) { return "site-" + i; })
+            .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
+            .attr("x", function (d, i) { return i%80 * 12 ; })
+            .style("fill", costColor);
 
-      d3.select("#tax")
-         .append("svg")
-         .selectAll("rect")
-         .data(data.prob)
-         .enter()
-         .append("rect")
-         .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
-         .attr("x", function (d, i) { return i%80 * 12 ; })
-         .style("fill", taxColor);
+        d3.select("#tax")
+            .selectAll("rect")
+            .data(data.prob)
+            .enter()
+            .append("rect")
+            .attr("class", function (d, i) { return "site-" + i; })
+            .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
+            .attr("x", function (d, i) { return i%80 * 12 ; })
+            .style("fill", taxColor);
 
-     d3.select("#oil")
-        .append("svg")
-        .selectAll("rect")
-        .data(data.prob)
-        .enter()
-        .append("rect")
-        .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
-        .attr("x", function (d, i) { return i%80 * 12 ; })
-        .style("fill", function (d) { return d == 0 ? 'black' : oilColor(d); });
+        d3.select("#oil")
+            .selectAll("rect")
+            .data(data.prob)
+            .enter()
+            .append("rect")
+            .attr("class", function (d, i) { return "site-" + i; })
+            .attr("y", function (d, i) { return Math.floor(i/80) * 18; })
+            .attr("x", function (d, i) { return i%80 * 12 ; })
+            .style("fill", function (d) { return d == 0 ? 'black' : oilColor(d); });
 
         d3.select("#fact").text(data.fact);
         d3.select("#week").text("Week " + data.week)
+    }).done(function () {
+        if ($(".cursor").length == 0) {
+            $(".site-0").toggleClass("cursor");
+        }
+        $("#survey").show()
     });
-
-    $("#survey").show()
-
-    return surveyState;
+    return survey;
 }
 
-function surveyState(game) {
+function survey() {
     $("#survey").hide()
     $("#report").show()
 
-    return reportState;
+    return report;
 }
 
-function reportState(game) {
+function report() {
     $("#report").hide()
     $("#drill").show()
 
-    return sellState;
+    return drill;
 }
 
-function drillState(game) {
+function drill() {
     $("#drill").hide()
     $("#sell").show()
 
-    return sellState;
+    return sell;
 }
 
-function sellState(game) {
+function sell() {
     $("#sell").hide()
     $("#lobby").show()
 
-    return lobbyState;
-}
-
-var curState = lobbyState;
-
-function move() {
-    curState = curState();
+    return lobby;
 }
 
 // % operator in javascript is remainder and isn't helpful for wrapping negatives
 function mod(a, n) {
     return a - (n * Math.floor(a/n));
+}
+
+function playState() {
+    var state = play;
+
+    return function() {
+        state = state();
+    }
 }
 
 function viewState() {
@@ -111,19 +119,54 @@ function viewState() {
 
     return function(delta) {
         $(views[cur]).hide();
-        cur = mod((cur + delta), views.length);
+        cur = mod(cur+delta, views.length);
         $(views[cur]).show();
     }
 }
 
-var view = viewState();
+function cursorState() {
+    var y = 0;
+    var x = 0;
 
-Mousetrap.bind('space', move);
+    return function(dy, dx) {
+        $(".site-" + (y * 80 + x)).toggleClass("cursor");
+
+        y = mod(y+dy, 24)
+        x = mod(x+dx, 80)
+
+        $(".site-" + (y * 80 + x)).toggleClass("cursor");
+    }
+}
+
+var play = playState();
+var view = viewState();
+var cursor = cursorState();
+
+Mousetrap.bind('space', play);
 Mousetrap.bind('tab', function(e) {
-    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-    return view(1)
-    });
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    view(1);
+});
 Mousetrap.bind('shift+tab', function(e) {
-    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-    return view(-1);
-    });
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    view(-1);
+});
+Mousetrap.bind('left', function(e) {
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    cursor(0, -1);
+});
+Mousetrap.bind('down', function(e) {
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    cursor(1, 0);
+});
+Mousetrap.bind('right', function(e) {
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    cursor(0, 1);
+})
+Mousetrap.bind('up', function(e) {
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    cursor(-1, 0);
+});
+
+// set everything in motion
+play();
